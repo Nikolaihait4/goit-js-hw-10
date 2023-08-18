31e3a30f7e1c365ab7f7e1884e68a733184137e6
Nikolaihait4 <jet1nikolai@gmail.com>
2023-08-18 09:08:04 +0200

    bugfix

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,23 +3,22 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_4MuXXvfWSGK2qNc7r2Qx38v1LkY3nYUbYW6BhNzl1z7qmVvGRaWlZTfPuvcGApYe';
 
-// Функция для получения списка пород
-export function fetchBreeds() {
-  return axios
-    .get('https://api.thecatapi.com/v1/breeds')
-    .then(response => response.data)
-    .catch(error => {
-      throw error;
-    });
+export async function fetchBreeds() {
+  try {
+    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
-// Функция для получения информации о коте по породе
-export function fetchCatByBreed(breedId) {
-  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-  return axios
-    .get(url)
-    .then(response => response.data)
-    .catch(error => {
-      throw error;
-    });
+export async function fetchCatByBreed(breedId) {
+  try {
+    const response = await axios.get(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
